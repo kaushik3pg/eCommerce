3faa5c37e2c26e873da622294324b844fb1fa909
Piyush Kaushik <piyush.kaushik@office365.3pillarglobal.com>
2022-09-05 14:32:12 +0530

    CSS Variables for customisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg-color": "var(--primary-bg-color)",
+        "secondary-bg-color": "var(--secondary-bg-color)",
+        "primary-text-color": "var(--primary-text-color)",
+      },
+    },
   },
   plugins: [],
-}
+};

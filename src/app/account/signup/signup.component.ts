2e5d36365b19719a5455acc5dfcb43c91a3baa2b
Piyush Kaushik <piyush.kaushik@office365.3pillarglobal.com>
2022-09-20 15:01:18 +0530

    Reactive forms

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 
+
+
+const matchPassword = (): ValidatorFn  =>  {
+  return (control : AbstractControl) : ValidationErrors | null =>  {
+    const {password, confirmPassword} = control.value;
+    return password !== confirmPassword ? {passwordsDontMatch: true} : null;
+  
+  }
+    }
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  registerForm = new FormGroup({
+    name  : new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email : new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
+    password : new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirmPassword : new FormControl('', [Validators.required]),
+  }, {validators: [matchPassword()]});
 
-  constructor() { }
+  constructor() {
+ 
+   }
 
   ngOnInit(): void {
+
   }
+
+
+
 
 }

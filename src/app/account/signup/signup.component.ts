7813351afd0c52c24e1a300c448cb77cecf58176
Piyush Kaushik <piyush.kaushik@office365.3pillarglobal.com>
2022-09-20 17:50:28 +0530

    added form builder

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,37 +1,71 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
-
-
-const matchPassword = (): ValidatorFn  =>  {
-  return (control : AbstractControl) : ValidationErrors | null =>  {
-    const {password, confirmPassword} = control.value;
-    return password !== confirmPassword ? {passwordsDontMatch: true} : null;
-  
-  }
-    }
+const matchPassword = (): ValidatorFn => {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const { password, confirmPassword } = control.value;
+    return password !== confirmPassword ? { passwordsDontMatch: true } : null;
+    // if (password !== confirmPassword) {
+    //   console.log(control);
+    //   control.get('confirmPassword')?.setErrors({ invalid: true });
+    //   return { passwordsDontMatch: true };
+    // } else {
+    //   return null;
+    // }
+  };
+};
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-  registerForm = new FormGroup({
-    name  : new FormControl('', [Validators.required, Validators.minLength(3)]),
-    email : new FormControl('', [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
-    password : new FormControl('', [Validators.required, Validators.minLength(8)]),
-    confirmPassword : new FormControl('', [Validators.required]),
-  }, {validators: [matchPassword()]});
+  signupForm!: FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor() {
- 
-   }
-
-  ngOnInit(): void {
-
+  createForm() {
+    this.signupForm = this.fb.group({
+      name: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/),
+        ]),
+      ],
+      userPassword: this.fb.group(
+        {
+          password: [
+            '',
+            Validators.compose([Validators.required, Validators.minLength(8)]),
+          ],
+          confirmPassword: [
+            '',
+            Validators.compose([Validators.required, Validators.minLength(8)]),
+          ],
+        },
+        { validators: [matchPassword()] }
+      ),
+    });
   }
 
-
-
-
+  getConfirmPasswordError() {
+    if (this.signupForm.get('userPassword')?.hasError('passwordsDontMatch')) {
+      return 'pass not matcha';
+    } else return null;
+  }
+  ngOnInit(): void {
+    this.createForm();
+  }
 }

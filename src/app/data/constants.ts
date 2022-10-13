1f38a7609a41ad428ae3609aac1d03f7be72ac37
Piyush Kaushik <piyush.kaushik@office365.3pillarglobal.com>
2022-10-13 19:08:00 +0530

    Added service for wishlist interaction

--- src/app/data/constants.ts
+++ src/app/data/constants.ts
@@ -2,3 +2,5 @@ export const close_snackbar = 'CLOSE';
 export const snackbar_duration = 3000;
 export const item_added_to_cart = 'ITEM ADDED TO CART';
 export const item_removed_from_cart = 'ITEM REMOVED FROM CART';
+export const item_added_to_wishlist = 'ITEM ADDED TO WISHLIST';
+export const item_removed_from_wishlist = 'ITEM REMOVED FROM WISHLIST';
